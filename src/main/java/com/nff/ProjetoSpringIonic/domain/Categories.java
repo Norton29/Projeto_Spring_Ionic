171ab1e5025484587e5bcd6e89b6dc309418975f
Norton29 <norton290194@gmail.com>
2022-04-12 01:59:48 -0300

    Configurando banco H2

--- src/main/java/com/nff/ProjetoSpringIonic/domain/Categories.java
+++ src/main/java/com/nff/ProjetoSpringIonic/domain/Categories.java
@@ -1,6 +1,9 @@
 package com.nff.ProjetoSpringIonic.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +15,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Categories {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 }
