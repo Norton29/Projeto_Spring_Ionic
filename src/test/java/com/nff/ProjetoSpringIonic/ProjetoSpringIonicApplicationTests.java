a7ed6ace67f7b17afb2c434bb808ee06b13a7629
Norton29 <norton290194@gmail.com>
2022-04-11 22:26:36 -0300

    Commit inicial

---
+++ src/test/java/com/nff/ProjetoSpringIonic/ProjetoSpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nff.ProjetoSpringIonic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
