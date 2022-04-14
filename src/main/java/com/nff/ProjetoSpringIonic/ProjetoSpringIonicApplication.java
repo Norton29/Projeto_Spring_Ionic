d07d6a40eec1f166289eecbb02b1db589f1c9ba0
Norton29 <norton290194@gmail.com>
2022-04-14 01:59:10 -0300

    Instanciando objetos na classe main

--- src/main/java/com/nff/ProjetoSpringIonic/ProjetoSpringIonicApplication.java
+++ src/main/java/com/nff/ProjetoSpringIonic/ProjetoSpringIonicApplication.java
@@ -1,13 +1,33 @@
 package com.nff.ProjetoSpringIonic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSpringIonicApplication {
+import com.nff.ProjetoSpringIonic.Repositories.CategoryRepository;
+import com.nff.ProjetoSpringIonic.domain.Categories;
 
+@SpringBootApplication
+public class ProjetoSpringIonicApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categories cat1 = new Categories(null,"Informática");
+		Categories cat2 = new Categories(null,"Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
